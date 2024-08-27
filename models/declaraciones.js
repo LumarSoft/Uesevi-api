031c3d3f3fd7288a196f1419eeaf30bd623b4837
marcebenitez2 <marcebenitez0607@gmail.com>
2024-08-27 12:45:36 -0300

    feat: trae las declaraciones por modified

--- models/declaraciones.js
+++ models/declaraciones.js
@@ -5,7 +5,7 @@ const declaracionesModel = {
     const query = `
       SELECT dj.*, e.nombre AS nombre_empresa, e.cuit AS cuit_empresa
       FROM declaraciones_juradas dj
-      INNER JOIN empresas e ON dj.empresa_id = e.id
+      INNER JOIN empresas e ON dj.empresa_id = e.id ORDER BY dj.modified DESC
     `;
     const [results] = await pool.query(query);
     return results;
